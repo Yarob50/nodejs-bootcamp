8f8918c63ed2ea0f69fa961de0a9bed4c7a7f57a
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-04-30 13:59:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// step: 1
+const express = require("express");
+
+// step: 2
+const app = express();
+
+// step: 4
+app.get("/h", function (req, res) {
+	res.send("welcome to the javascript bootcamp");
+});
+
+app.get("/name", function (req, res) {
+	res.send("Yarob");
+});
+
+// step: 3
+app.listen(3300, function () {
+	console.log("listening");
+});
