0b781e988179c7c3b05796a4d95470548d26ed9d
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2023-05-14 13:16:42 +0300

    50%

--- app.js
+++ app.js
@@ -5,20 +5,8 @@ const User = require("./models/User");
 const bodyParser = require("body-parser");
 const Profile = require("./models/Profile");
 const Tag = require("./models/Tag");
-
-// requiring cookies & sessions:
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-
-const app = express();
-
-// using the cookies & sessions
-app.use(cookieParser());
-app.use(
-	session({
-		secret: "my secret",
-	})
-);
 
 mongoose
 	.connect(
@@ -30,98 +18,95 @@ mongoose
 	.catch((err) => {
 		console.log(err);
 	});
+const app = express();
+
+app.use(
+	session({
+		secret: "my secret",
+	})
+);
+
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/register", (req, res) => {
-	const username = req.query.username;
-	const email = req.query.email;
-	const password = req.query.password;
-
-	const user = new User({
-		username: username,
-		password: password,
-		email: email,
-	});
-
-	user.save().then(() => {
-		res.send("saved");
-	});
-	// res.send(username + " " + email + " " + password);
-});
-
-app.get("/login", (req, res) => {
+app.get("/loginUser", (req, res) => {
 	const username = req.query.username;
 	const password = req.query.password;
-	const email = req.query.email;
 
-	// res.send(typeof username + " " + typeof password);
-	// return;
-	User.findOne({ username: username }, { username: 1, password: 1 })
-		.then((foundUser) => {
-			// res.send("test");
-			// if (Object.keys(foundUser).length === 0) {
-			// 	res.send(foundUser);
-			// } else {
-			// 	res.send("error");
-			// }
-
-			console.log("===========");
-			console.log(foundUser);
+	User.findOne({ username: username, password: password }).then(
+		(foundUser) => {
 			if (foundUser) {
-				req.session.userId = foundUser._id;
+				req.session.currentuser = foundUser._id;
 				res.send(foundUser);
 			} else {
 				res.send("error");
 			}
-			// if (foundUser) {
-			// 	res.send(foundUser);
-			// } else {
-			// 	res.send("credentials are incorrect");
-			// }
-		})
-		.catch((err) => {
-			res.send(err);
-		});
-	// res.send(username + " " + password);
+		}
+	);
 });
-app.get("/secret", (req, res) => {
-	if (req.session.userId) {
-		res.send("JS bootcamp");
-	} else {
-		res.send("you are not allowed, please login first");
-	}
+
+app.get("/register", (req, res) => {
+	let username = req.query.username;
+	let password = req.query.password;
+	let email = req.query.email;
+
+	User.create({
+		username: username,
+		email: email,
+		password: password,
+	}).then((createdUser) => {
+		res.send("created successfully");
+	});
+});
+
+app.get("/loggin", (req, res) => {
+	const username = req.query.username;
+	const password = req.query.password;
+
+	User.findOne({ username: username, password: password }).then(
+		(foundUser) => {
+			if (foundUser) {
+				req.session.currentuser = foundUser._id;
+				res.send(foundUser);
+			} else {
+				res.send("error");
+			}
+		}
+	);
 });
 
 app.get("/logout", (req, res) => {
 	req.session.destroy();
-	res.send("logged out success");
+	res.send("logged out");
 });
 
-app.get("/session", (req, res) => {
-	res.send("visiting session");
-});
-
-app.get("/addToCart", (req, res) => {
-	if (req.session.numberOfItems) {
-		req.session.numberOfItems += 1;
+app.get("/secret", (req, res) => {
+	if (req.session.currentUser) {
+		res.send("hello world");
 	} else {
-		req.session.numberOfItems = 1;
+		res.send("not authorized");
 	}
+});
 
+app.get("/secret2", (req, res) => {
+	if (req.session.currentUser) {
+		res.send("hello world2222");
+	} else {
+		res.send("not authorized");
+	}
+});
+
+// SESSION
+app.get("/session", (req, res) => {
+	req.session.username = "Yarob";
+	res.send(req.headers);
+	// res.send(res.getHeaders());
+});
+
+app.get("/session2", (req, res) => {
+	res.cookie("test", "hello");
 	res.send(res.getHeaders());
-	return;
-	req.session.name = "js course";
-	res.send("done");
-});
-
-app.get("/numberOfItems", (req, res) => {
-	res.send(req.session);
-});
-
-app.get("/xyz", (req, res) => {
-	req.session.destroy();
-	res.send("done");
 });
 
 app.get("/getSpecificUser", (req, res) => {
